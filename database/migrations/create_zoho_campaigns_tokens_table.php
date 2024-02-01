c1c88edf83972605cb9af086ec318ae550b13876
Fabio Capucci <fabio.cappuc@gmail.com>
2024-02-01 12:26:14 +0100

    added down migration

--- database/migrations/create_zoho_campaigns_tokens_table.php
+++ database/migrations/create_zoho_campaigns_tokens_table.php
@@ -4,9 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up()
+return new class extends Migration {
+    public function up(): void
     {
         Schema::create('zoho_campaigns_tokens', function (Blueprint $table) {
             $table->id();
@@ -15,5 +14,10 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::drop('zoho_campaigns_tokens');
     }
 };
