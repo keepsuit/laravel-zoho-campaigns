a41f2b60f8b634b49c593a0476b484d776615133
Andy <75330721+andreibaca@users.noreply.github.com>
2024-05-27 01:40:17 +0300

    fix(Campaings): return from subscribers if range is 0

--- src/Campaigns.php
+++ src/Campaigns.php
@@ -84,7 +84,8 @@ class Campaigns
         return LazyCollection::make(function () use ($status, $sort, $listKey, $maxRange) {
             $fromIndex = 1;
             $range = $maxRange ?? $this->zohoApi->listSubscribersCount($listKey, status: $status);
-
+            if (empty($range)) return;
+            
             while (true) {
                 $response = $this->zohoApi->listSubscribers($listKey, status: $status, sort: $sort, fromIndex: $fromIndex, range: $range);
 
